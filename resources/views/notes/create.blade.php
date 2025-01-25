2ef00f3a6e24f11a2be0e31633f87deb3ac667bc
Santiago Graciano <santiago.gracianod@gmail.com>
2025-01-25 09:45:24 -0500

    alert with error

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -4,6 +4,17 @@
 @section('content')
 <a href={{ route('note.index') }}>Back</a>
 <h1>Create Note</h1>
+
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 <form action="{{ route('note.store') }}" method="POST">
     @csrf
     <label for="title">Title</label>
@@ -11,4 +22,5 @@
     <label for="description">Description</label>
     <input type="text" name="description" id="description" >
     <button type="submit">Create Note</button>
+</form>
 @endsection
--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -5,13 +5,24 @@
 @section('content')
     <a href={{ route('note.index') }}>Back</a>
     <h1>Edit note</h1>
-    <form action={{ route('note.update', $note->id) }} method="POST">
-        @method('PUT')
-        @csrf
-        @method('PUT')
-        <label for="title">Title</label>
-        <input type="text" name="title" id="title" value="{{ $note->title }}">
-        <label for="description">Description</label>
-        <input type="text" name="description" id="description" value="{{ $note->description }}">
-        <button type="submit">Update note</button>
+
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+<form action={{ route('note.update', $note->id) }} method="POST">
+    @method('PUT')
+    @csrf
+    @method('PUT')
+    <label for="title">Title</label>
+    <input type="text" name="title" id="title" value="{{ $note->title }}">
+    <label for="description">Description</label>
+    <input type="text" name="description" id="description" value="{{ $note->description }}">
+    <button type="submit">Update note</button>
+</form>
 @endsection
